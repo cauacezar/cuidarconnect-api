6bddbfdda369ee4019d3a76ffa097cdf99f0317d
cauacezar <caua.cs.alves@gmail.com>
2026-02-11 16:14:16 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
 const { Pool } = require("pg");
 
-const connectionString = process.env.DATABASE_URL;
-
 const pool = new Pool({
-  connectionString,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  host: process.env.PGHOST,
+  port: Number(process.env.PGPORT || 5432),
+  database: process.env.PGDATABASE,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  ssl: { rejectUnauthorized: false },
 });
 
 module.exports = pool;
