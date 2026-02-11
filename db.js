f5899945b6b05641d2ca85036b4832d5f64280be
cauacezar <caua.cs.alves@gmail.com>
2026-02-11 15:46:28 -0300

    Update db.js

--- db.js
+++ db.js
@@ -2,15 +2,11 @@ const { Pool } = require("pg");
 
 const connectionString = process.env.DATABASE_URL;
 
-if (!connectionString) {
-  console.warn("⚠️ DATABASE_URL não definido. Configure no .env (local) e no Render (Environment).");
-}
-
 const pool = new Pool({
   connectionString,
-  ssl: connectionString && connectionString.includes("sslmode=require")
-    ? { rejectUnauthorized: false }
-    : false,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
